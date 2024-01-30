78a132514a67e61227c60c57fcafcf11606eda81
Mary Immanuel Joseph <36118884+maryimjoseph@users.noreply.github.com>
2024-01-29 23:10:39 -0800

    Update find_factorial.py
    Included input validation to accept only non-negative integers.
    Also added changes to return the result for zero factorial.

--- day_19/find_factorial.py
+++ day_19/find_factorial.py
@@ -3,7 +3,7 @@
 # This script uses recursion to find the factorial of a given number.
 
 def find_factorial(p_num):
-  if p_num == 1:
+  if p_num == 1 or p_num == 0:
     return 1
   return p_num * find_factorial(p_num - 1)
 
@@ -11,9 +11,11 @@ if __name__ == '__main__':
   while(True):
     try:
       v_num = int(input('Please enter a number(integer) : '))
+      if v_num < 0:
+        print('Please enter a non-negative number\n')
+        continue
       break
     except ValueError:
       print('Please enter an integer value\n')
 
   print(f'Factorial of {v_num} is : ', find_factorial(v_num))
-  
